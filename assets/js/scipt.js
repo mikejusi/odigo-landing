ebfaa0f810bb068b7d21300e552aef7622fce482
Jusi <megamahmutpower@gmail.com>
2022-09-11 15:07:24 +0300

    scroll to top btn

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -2,3 +2,14 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+window.addEventListener("scroll", function(){
+    var totop = document.querySelector(".btn__scroll__top");
+    totop.classList.toggle("btn__scroll__top__active", window.scrollY > 0)
+})
+
+$('#btn__scroll__top').click(function() {
+    $('body,html').animate({
+        scrollTop : 0
+    }, 500);
+});
